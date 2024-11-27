3e2b5843cb72c69da608da22644ec14467050717
rogerin07 <rogersantosbezerra@gmail.com>
2024-11-26 23:04:51 -0300

    segundo

--- siganet.py
+++ siganet.py
@@ -3,77 +3,70 @@ import time
 import tkinter as tk
 from tkinter import ttk
 
-class Servidores:
-    def _init_(self,nome,ip,):
+class Servidores():
+    def __init__(self,nome,ip,):
         self.nome = nome
         self.ip = ip
 
 class Taubate(Servidores):
-    def _init_(self,nome,ip):
-        super()._init_(nome,ip)
-        self.nome = "Taubate"
-        self.ip = "xxxxxxxxxxxx"
+    def __init__(self):
+        super().__init__("Taubate", "xxxxxxxxxxxxx")
 
 class Charles(Servidores):
-    def _init_(self,nome,ip):
-        super()._init_(nome,ip)
-        self.nome = "Charles"
-        self.ip = "xxxxxxxxxxxx"
+    def __init__(self):
+        super().__init__("Charles", "xxxxxxxxxxxxx")
 
 class Caragua(Servidores):
-    def _init_(self,nome,ip):
-        super()._init_(nome,ip)
-        self.nome = "Caragua"
-        self.ip = "xxxxxxxxxxxx"
-
+    def __init__(self):
+       super().__init__("Caragua", "xxxxxxxxxxxxx")
 class Jacarei(Servidores):
-    def _init_(self,nome,ip):
-        super()._init_(nome,ip)
-        self.nome = "Jacarei"
-        self.ip = "xxxxxxxxxxxx"
+    def __init__(self):
+        super().__init__("Jacarei", "xxxxxxxxxxxxx")
 
 class Siqueira(Servidores):
-    def _init_(self,nome,ip):
-        super()._init_(nome,ip)
-        self.nome = "Siqueira"
-        self.ip = "xxxxxxxxxxxx"
+    def __init__(self):
+        super().__init__("Siqueira", "xxxxxxxxxxxxx")
 
 class Vista_Verde(Servidores):
-    def _init_(self,nome,ip):
-        super()._init_(nome,ip)
-        self.nome = "Vista Verde"
-        self.ip = "xxxxxxxxxxxx"
+    def __init__(self):
+        super().__init__("Vista_Verde", "xxxxxxxxxxxxx")
 
 class Vila_Industrial(Servidores):
-    def _init_(self,nome,ip):
-        super()._init_(nome,ip)
-        self.nome = "Vila Industrial"
-        self.ip = "xxxxxxxxxxxx"
+    def __init__(self):
+        super().__init__("Vila_Industrial", "xxxxxxxxxxxxx")
 
 class Vila_Adyanna(Servidores):
-    def _init_(self,nome,ip):
-        super()._init_(nome,ip)
-        self.nome = "Vila Adyanna"
-        self.ip = "xxxxxxxxxxxx"
+    def __init__(self):
+        super().__init__("Vila_Adyanna", "xxxxxxxxxxxxx")
 
 class Bosque(Servidores):
-    def _init_(self,nome,ip):
-        super()._init_(nome,ip)
-        self.nome = "Bosque"
-        self.ip = "xxxxxxxxxxxx"
+    def __init__(self):
+        super().__init__("Bosque", "xxxxxxxxxxxxx")
 
 class Satelite(Servidores):
-    def _init_(self,nome,ip):
-        super()._init_(nome,ip)
-        self.nome = "Satelite"
-        self.ip = "xxxxxxxxxxxx"
+    def __init__(self):
+        super().__init__("Satelite", "xxxxxxxxxxxxx")
 
 class Matriz(Servidores):
-    def _init_(self,nome,ip):
-        super()._init_(nome,ip)
-        self.nome = "Matriz"
-        self.ip = "xxxxxxxxxxxx"
+    def __init__(self):
+        super().__init__("Matriz", "xxxxxxxxxxxxx")
 
+janela = tk.Tk()
+janela.title("siganet")
+
+Servidores = [
+    Taubate(), Charles(), Caragua(), Jacarei(), Siqueira(), Vista_Verde(), Vila_Industrial(), Vila_Adyanna(), Bosque(), Satelite(), Matriz()
+]
+
+for servidor in Servidores:
+    btn = tk.Button(
+        janela, 
+        text=servidor.nome, 
+        width = 20, height = 2
+    )
+    btn.pack(pady=5)
+
+janela.mainloop()
 
 
     
